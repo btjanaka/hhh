70ff5c8d35ddc2c326b48fa797eda04cda0ec4bf
btjanaka <bryon@btjanaka.net>
2020-05-27 17:58:20 -0700

    Remove Basic Models

--- hhh/models/__init__.py
+++ hhh/models/__init__.py
@@ -1,5 +1,3 @@
 """All bird audio detection models."""
-from hhh.models.basic_model import BasicModel
-from hhh.models.basic_smaller_model import BasicSmallerModel
 from hhh.models.conv_nets import BabyConvNet, ConvNet
 from hhh.models.multi_feature_model import MultifeatureModel
